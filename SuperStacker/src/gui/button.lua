67281afa04e8b5ba3e73694fdfc1ff4475fe6efa
aiden <changaiden19@gmail.com>
2026-03-13 20:35:58 -0700

    Button GUI In progress

--- SuperStacker/src/gui/button.lua
+++ SuperStacker/src/gui/button.lua
@@ -1,13 +1,26 @@
-local button = {}
+local element = require("src.gui.element")
+
+local button = setmetatable({}, {__index = element})
 button.__index = button
 
-function button.new(x, y, w, h)
-    local self = {}
-    setmetatable(self, button)
+function button.new(gui, x, y, w, h)
+    local self = setmetatable(button.new(gui, x, y, w, h), button)
 
-    self.x = 0
+    self.color = {0, 0, 0}
+    self.normalcolor = {0, 0, 0}
+    self.hovercolor = {0.5, 0.5, 0.5}
+    self.pressedcolor = {1, 1, 1}
 
     return self
 end
+
+function button:update(mx, my)
+    element.update(self, mx, my)
+end
+
+function button:draw()
+    self.gui:drawrectangle(x, y,)
+end
+
 
 return button
